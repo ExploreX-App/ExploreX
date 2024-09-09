896b222d648fe529587e486690702bfc8e9a6d96
Amy Ahn <amy.ahn.kr@gmail.com>
2024-09-09 10:27:22 -0400

    test

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import { useActivitiesQuery } from './hooks/useFetchActivities';
 import HomePage from './pages/HomePage/HomePage';
 import AppLayout from './layout/AppLayout';
 import ActivityDetailPage from './pages/ActivityDetailPage/ActivityDetailPage';
@@ -10,15 +9,15 @@ import ActivityPage from './pages/ActivityPage/ActivityPage';
 function App() {
   return (
     <Routes>
-      <Route path="/" element={<AppLayout />}>
+      <Route path='/' element={<AppLayout />}>
         <Route index element={<HomePage />} />
-        <Route path="activities">
+        <Route path='activities'>
           <Route index element={<ActivityPage />} />
-          <Route path=":id" element={<ActivityDetailPage />} />
+          <Route path=':id' element={<ActivityDetailPage />} />
         </Route>
       </Route>
 
-      <Route path="*" element={<NotFoundPage />} />
+      <Route path='*' element={<NotFoundPage />} />
     </Routes>
   );
 }
