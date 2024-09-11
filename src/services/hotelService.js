7571ac6e47643976372896ddf4170950c090c64c
Suhyun <suhyun.park63@gmail.com>
2024-09-11 05:03:16 -0700

    added hotel detail hooks

--- src/services/hotelService.js
+++ src/services/hotelService.js
@@ -1,11 +1,14 @@
 import hotelApi from "../api/hotelAPI";
 
-export const fetchHotelDestination = async ({ keyword }) => {
-    try {
-      const response = await hotelApi.get(`/searchDestination?query=${keyword}`);
-      return response.data;
-    } catch (error) {
-      console.error("Error fetching destination with keyword:", error);
-      throw error;
-    }
-  };
+const fetchHotelDestination = async ({ keyword }) => {
+  try {
+    const response = await hotelApi.get(`/searchDestination?query=${keyword}`);
+    return response.data;
+  } catch (error) {
+    console.error("Error fetching destination with keyword:", error);
+    throw error;
+  }
+};
+
+
+export { fetchHotelDestination };
