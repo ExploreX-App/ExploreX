932c8812c16a23094822b8e464156d7ee2e92a61
Minjoo Kim <haileykim206@gmail.com>
2024-09-11 23:38:03 -0700

    edit search bar, card css

--- src/common/SearchBar/SearchBar.jsx
+++ src/common/SearchBar/SearchBar.jsx
@@ -1,10 +1,9 @@
-import React, { useState, useCallback } from "react";
-import { Tabs, Tab, Container, Row, Col, Button } from "react-bootstrap";
+import React, { useState } from "react";
+import { Tabs, Tab, Container, Row } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./SearchBar.style.css";
-import CalendarPicker from "./component/CalendarPicker/CalendarPicker";
-import CountrySearch from "./component/CountrySearch/CountrySearch";
-import VisitorCounter from "./component/VisitorCounter/VisitorCounter";
+import HotelSearch from "./component/HotelSearch/HotelSearch";
+import ActivitySearch from "./component/ActivitySearch/ActivitySearch";
 
 const SearchBar = () => {
   const [country, setCountry] = useState("");
@@ -14,17 +13,11 @@ const SearchBar = () => {
     totalTravelers: 2,
     totalRooms: 1,
   });
-
-  const [activeTab, setActiveTab] = useState("hotel")
+  const [activeTab, setActiveTab] = useState("hotel");
 
   const handleCountryChange = (selectedCountry) => {
     setCountry(selectedCountry);
   };
-
-  const handleDateChange = useCallback(({ dateFrom, dateTo }) => {
-    setDateFrom(dateFrom);
-    setDateTo(dateTo);
-  },[]);
 
   const updateVisitorInfo = (travelers, rooms) => {
     setVisitorInfo({ totalTravelers: travelers, totalRooms: rooms });
@@ -35,74 +28,54 @@ const SearchBar = () => {
       country,
       dateFrom,
       dateTo,
-   
     };
-if (activeTab === "hotel") {
-    searchData.visitors = visitorInfo.totalTravelers;
-  searchData.rooms = visitorInfo.totalRooms;
-   performHotelSearch(searchData);
-  } else if (activeTab === "activity") {
-    performActivitySearch(searchData)
+
+    if (activeTab === "hotel") {
+      searchData.visitors = visitorInfo.totalTravelers;
+      searchData.rooms = visitorInfo.totalRooms;
+      performHotelSearch(searchData);
+    } else if (activeTab === "activity") {
+      performActivitySearch(searchData);
     }
   };
-  
 
   const performHotelSearch = (searchData) => {
-    console.log("hotel Search:", searchData)
-  }
-  const performActivitySearch = (searchData) => {
-    console.log("Activity Search:", searchData)
-  }
+    console.log("Hotel Search:", searchData);
+  };
 
+  const performActivitySearch = (searchData) => {
+    console.log("Activity Search:", searchData);
+  };
 
   return (
-    <div>
-      <Tabs defaultActiveKey="hotel" id="uncontrolled-tab-example" className="mb-3" onSelect={(selectedTab)=> setActiveTab(selectedTab)}>
+    <div className="search-container">
+      <Tabs
+        defaultActiveKey="hotel"
+        id="fill-tab-example"
+        className="mb-3"
+        fill
+        onSelect={(selectedTab) => setActiveTab(selectedTab)}
+      >
         <Tab eventKey="hotel" title="Hotel">
           <Container>
             <Row>
-              <Col lg={3} sm={12}>
-                <CountrySearch onCountryChange={handleCountryChange} />
-              </Col>
-              <Col lg={3} sm={12}>
-                <CalendarPicker onDateChange={handleDateChange} />
-              </Col>
-              <Col lg={4} sm={12}>
-                <VisitorCounter onVisitorInfoChange={updateVisitorInfo} />
-              </Col>
-              <Col lg={2} sm={12}>
-                <Button className="search-btn" variant="primary" onClick={handleSearch}>
-                  Search
-                </Button>
-              </Col>
+              <HotelSearch 
+                onCountryChange={handleCountryChange} 
+                onVisitorInfoChange={updateVisitorInfo}
+                onSearch={handleSearch} 
+              />
             </Row>
           </Container>
         </Tab>
         <Tab eventKey="activity" title="Activity">
-          
           <Container>
             <Row>
-              <div className="activity-search">
-            <Col lg={4} sm={12}>
-                  <CountrySearch onCountryChange={handleCountryChange} />
-              </Col>
-              <Col lg={5} sm={12}>
-                <CalendarPicker onDateChange={handleDateChange} />
-              </Col>
-              <Col lg={2} sm={12}>
-                <Button className="search-btn" variant="primary" onClick={handleSearch}>
-                  Search
-                </Button>
-                </Col>
-              </div>
+              <ActivitySearch 
+                onCountryChange={handleCountryChange} 
+                onSearch={handleSearch} 
+              />
             </Row>
           </Container>
-
-              
-              
-        </Tab>
-        <Tab eventKey="contact" title="Contact" disabled>
-          Tab content for Contact
         </Tab>
       </Tabs>
     </div>
