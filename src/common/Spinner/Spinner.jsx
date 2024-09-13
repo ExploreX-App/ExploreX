ed3f2d24954ce0ae94b438dd10140cee2907bf13
Amy Ahn <amy.ahn.kr@gmail.com>
2024-09-13 15:52:40 -0400

    spinner completed

--- src/common/Spinner/Spinner.jsx
+++ src/common/Spinner/Spinner.jsx
@@ -1,23 +1,26 @@
 import React from 'react';
-import './Spinner.style.css'
+import './Spinner.style.css';
 
 const Spinner = () => {
   return (
-    <div className="loadingio-spinner-spinner-2by998twmg8">
-      <div className="ldio-yzaezf3dcmj">
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
-        <div></div>
+    <div className='spinner-container'>
+      <div className='loadingio-spinner-spinner-2by998twmg8'>
+        <div className='ldio-yzaezf3dcmj'>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
       </div>
+      <div className='loading-text'>Loading...</div>
     </div>
   );
 };
