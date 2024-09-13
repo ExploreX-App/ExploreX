9cecdaa195e7f29afa613c55e2f5d582bf0a9f41
Amy Ahn <amy.ahn.kr@gmail.com>
2024-09-13 17:17:07 -0400

    FAQ updated

--- src/pages/ActivityDetailPage/components/FAQSection/FAQSection.jsx
+++ src/pages/ActivityDetailPage/components/FAQSection/FAQSection.jsx
@@ -1,11 +1,57 @@
 import React from 'react';
-import { Button } from 'react-bootstrap';
+import { Accordion } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './FAQSection.style.css';
 
 const FAQSection = () => (
   <div>
-    <h3>Have a question?</h3>
-    <input type="text" placeholder="Ask your question..." className="faq-search-bar" />
-    <Button variant="primary" className="ml-2">Search</Button>
+    <h1 className='FAQ-title'>
+      <img
+        src='https://cdn.dribbble.com/userupload/12999996/file/original-4f6387877d5fe6775f746cfbec15a6eb.jpg?resize=752x'
+        style={{ width: '150px' }}
+        alt='Frequently Asked Questions Illustration'
+      />
+      Frequently Asked Questions
+    </h1>
+
+    <Accordion defaultActiveKey={['0']} alwaysOpen>
+      <Accordion.Item eventKey='0'>
+        <Accordion.Header>What servivces do you provide?</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='1'>
+        <Accordion.Header>Cancellation policy</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+      <Accordion.Item eventKey='2'>
+        <Accordion.Header>What to expect from this activity</Accordion.Header>
+        <Accordion.Body>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+          eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
+          minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+          aliquip ex ea commodo consequat. Duis aute irure dolor in
+          reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
+          pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+          culpa qui officia deserunt mollit anim id est laborum.
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
   </div>
 );
 
