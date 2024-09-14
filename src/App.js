1b3bab6d056460ea04483bc5866d252b056aeb44
Amy Ahn <amy.ahn.kr@gmail.com>
2024-09-14 19:43:11 -0400

    about update

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import { Route, Routes } from "react-router-dom";
-import "./App.css";
-import HomePage from "./pages/HomePage/HomePage";
-import AppLayout from "./layout/AppLayout";
-import ActivityDetailPage from "./pages/ActivityDetailPage/ActivityDetailPage";
-import NotFoundPage from "./pages/NotFoundPage/NotFoundPage";
-import ActivityPage from "./pages/ActivityPage/ActivityPage";
-import HotelPage from "./pages/HotelPage/HotelPage";
-import AboutPage from "./pages/About/AboutPage.tsx";
-import LoginPage from "./pages/LoginPage/LoginPage";
-import HotelDetailPage from "./pages/HotelDetailPage/HotelDetailPage";
-import ProfilePage from "./pages/ProfilePage/ProfilePage";
-import HotelBookPage from "./pages/HotelBookPage/HotelBookPage";
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import HomePage from './pages/HomePage/HomePage';
+import AppLayout from './layout/AppLayout';
+import ActivityDetailPage from './pages/ActivityDetailPage/ActivityDetailPage';
+import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
+import ActivityPage from './pages/ActivityPage/ActivityPage';
+import HotelPage from './pages/HotelPage/HotelPage';
+import AboutPage from './pages/About/AboutPage.tsx';
+import LoginPage from './pages/LoginPage/LoginPage';
+import HotelDetailPage from './pages/HotelDetailPage/HotelDetailPage';
+import ProfilePage from './pages/ProfilePage/ProfilePage';
+import HotelBookPage from './pages/HotelBookPage/HotelBookPage';
 
 function App() {
   return (
@@ -24,17 +24,11 @@ function App() {
         </Route>
         <Route path='hotels'>
           <Route index element={<HotelPage />} />
-<<<<<<< HEAD
           <Route path=':id' element={<HotelDetailPage />} />
+          <Route path=':id/reserve' element={<HotelBookPage />} />
         </Route>
         <Route path='login' element={<LoginPage />} />
-=======
-          <Route path=":id" element={<HotelDetailPage />} />
-          <Route path=":id/reserve" element={<HotelBookPage />} />
-        </Route>
-        <Route path="login" element={<LoginPage />} />
-        <Route path="profile" element={<ProfilePage />} />
->>>>>>> develop
+        <Route path='profile' element={<ProfilePage />} />
       </Route>
 
       <Route path='*' element={<NotFoundPage />} />
---
+++ src/utils/mockData/teamMembersInfo.js
@@ -0,0 +1,38 @@
+import member1 from '../../assets/member1.jpg';
+import member2 from '../../assets/member2.jpg';
+import member3 from '../../assets/member3.png';
+import member4 from '../../assets/member1.jpg';
+import member5 from '../../assets/member1.jpg';
+
+export const teamMembers = [
+  {
+    imgSrc: member1,
+    name: 'Suhyun Park',
+    title: 'Project Owner',
+    role: 'Lead responsibility for project management'
+  },
+  {
+    imgSrc: member2,
+    name: 'James Jo',
+    title: 'Scrum Master',
+    role: 'Manage the Agile process'
+  },
+  {
+    imgSrc: member3,
+    name: 'Hailey Kim',
+    title: 'UX Designer / Developer',
+    role: 'Responsible for the user interface design'
+  },
+  {
+    imgSrc: member4,
+    name: 'Amy Ahn',
+    title: 'Designer / Developer',
+    role: 'Design and development support'
+  },
+  {
+    imgSrc: member5,
+    name: 'May Kim',
+    title: 'Main Developer',
+    role: 'Lead development efforts'
+  }
+];
